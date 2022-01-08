1376b638ead9d5340ab2d802701aa922785cf1c1
Nilton Firmino <samuraijack2000@gmail.com>
2022-01-08 06:40:56 -0500

    ProblemsDetailsView

---
+++ Modulo3NativoIOS/ProblemAddEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProblemAddEditViewController.swift
+//  Modulo3NativoIOS
+//
+//  Created by user211591 on 1/8/22.
+//
+
+import UIKit
+
+class ProblemAddEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Modulo3NativoIOS/ProblemDetailsViewController.swift
+++ Modulo3NativoIOS/ProblemDetailsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProblemDetailsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
