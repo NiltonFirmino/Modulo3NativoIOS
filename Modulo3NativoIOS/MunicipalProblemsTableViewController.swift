16c1b5bcdd9d0f73b86d9707c8eb126ca3323b16
Nilton Firmino <samuraijack2000@gmail.com>
2022-01-09 18:35:28 -0500

    Completed

--- Modulo3NativoIOS/MunicipalProblemsTableViewController.swift
+++ Modulo3NativoIOS/MunicipalProblemsTableViewController.swift
@@ -63,17 +63,16 @@ class MunicipalProblemsTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let mproblem = fetchedResultsController.object(at: indexPath)
+            context.delete(mproblem)
+            try? context.save()
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
--- Modulo3NativoIOS/ProblemAddEditViewController.swift
+++ Modulo3NativoIOS/ProblemAddEditViewController.swift
@@ -12,15 +12,73 @@ class ProblemAddEditViewController: UIViewController {
     
     @IBOutlet weak var LabelWhatProblem: UITextField!
     @IBOutlet weak var LabelProblemLocation: UITextField!
-    
     @IBOutlet weak var ButtonUpload: UIButton!
     @IBOutlet weak var ImageViewProblem: UIImageView!
+    @IBOutlet weak var TextViewDetails: UITextView!
+    
+    var mproblem: MunicipalProblem?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+    
+        if let mproblem = mproblem {
+            title = "Edição"
+            LabelWhatProblem = mproblem?.title
+            LabelProblemLocation = mproblem?.location
+            TextViewDetails = mproblem.detail
+        
+    }}
+
+    @IBAction func ButtonUploadImage(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Selecionar imagem", message: "De onde deseja escolher a imagem?", preferredStyle:.actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: _ in
+               self.selectPicture(sourceType: .camera)
+        )
+        alert.addAction(cameraAction)
+        
+        let libraryAction = UIAlertAction(title: "Biblioteca de Fotos", style: .default, handler: _ in
+           self.selectPicture(sourceType: .photoLibrary)        )
+        alert.addAction(libraryAction)
+        
+        let albumAction = UIAlertAction(title: "Album de Fotos", style: .default, handler: _ in
+           self.selectPicture(sourceType: .savedPhotosAlbum)        )
+        alert.addAction(albumAction)
+        
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
     }
     
-
+    func selectPicture(sourceType: UIImagePickerController.SourceType) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
     
+    @IBAction func ButtonDone(_ sender: Any) {
+        if mproblem == nil {
+            mproblem = MunicipalProblem(context: context)
+        }
+        
+        mproblem?.title = LabelWhatProblem.text
+        mproblem?.location = LabelProblemLocation.text
+        mproblem?.image = ImageViewProblem.image?.jpegData(compressionQuality: 0.8)
+        mproblem?.detail = TextViewDetails.text
+        
+        try? context.save()
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
 
+extension ProblemAddEditViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage{
+            ImageViewProblem.image = image
+        }
+    }
 }
